ae2c8955253659c39d8c7fe0359edb67e4201782
Marcelo Brito <marcelogbrito@gmail.com>
2026-04-14 22:24:56 -0300

    mudanças na emissao de atestados

--- electron/main.ts
+++ electron/main.ts
@@ -349,7 +349,7 @@ ipcMain.handle('gerar-laudos-lote', async (_, payload) => {
         ? path.join(process.resourcesPath, 'templates', laudo.arquivo) 
         : path.join(__dirname, '..', 'public', 'templates', laudo.arquivo);
 
-      const nomeArquivo = `Laudo_${laudo.nome}_${nomeAmigavel}.docx`;
+      const nomeArquivo = `Atestado_${laudo.nome}_${nomeAmigavel}.docx`;
       const outputPath = path.join(app.getPath('downloads'), nomeArquivo);
 
       await gerarLaudoWorker(dadosTemplate, templatePath, outputPath);
